b78e73f2d906908c9851f65658011d296dc6b66c
DureginKostya <kot-502@mail.ru>
2023-04-04 21:18:39 +0500

    Changed #3 files create_data.py, processing_writing.py, ui.py and my_phone_book.txt

--- Task38/processing_writing.py
+++ Task38/processing_writing.py
@@ -9,11 +9,23 @@ def import_writing():
                     phone_book.seek(0)
                     if writing not in phone_book:
                         phone_book.write(writing)
+        print('Данные перенесены', end='\n\n')
     except:
         print('Указанный файл не найден')
 
-# def export_writing():
-#     return
+def export_writing():
+    exp_path = input_path_file('Укажите путь и имя файла для экспорта данных: ')
+    path_phone_book = input_path_file('Укажите путь и имя экспортируемого файла: ')
+    try:
+        with open(path_phone_book.strip(), 'r', encoding='utf-8') as phone_book:
+            with open(exp_path.strip(), '+a', encoding='utf-8') as exp_file:
+                for writing in phone_book:
+                    exp_file.seek(0)
+                    if writing not in exp_file:
+                        exp_file.write(writing)
+        print('Данные перенесены', end='\n\n')
+    except:
+        print('Указанный файл не найден')
 
 def viewer_writing():
     try:
@@ -27,11 +39,59 @@ def viewer_writing():
     except:
         print('Телефонного справочника нет')
 
-# def add_writing():
-#     return
+def add_writing(key_add):
+    try:
+        with open('my_phone_book.txt', 'a', encoding='utf-8') as phone_book:
+            while key_add:
+                print('Добавьте контакт:')
+                surname = input_surname()
+                firstname = input_firstname()
+                patronymic = input_patronymic()
+                num_phone = input_num_phone()
+                while not num_phone.isdigit():
+                    print('''Номер телефона должен содержать только цифры!!!
+Введите повторно номер телефона''')
+                    num_phone = input_num_phone()
+                phone_book.write(f'{surname}!{firstname}!{patronymic}!{num_phone}\n')
+                print('Контакт добавлен')
+                sequel = input('Продолжить добавление контактов [yes/no]: ')
+                while not sequel in ('yes', 'no'):
+                    print('Введена неправильная команда, повторите ввод')
+                    sequel = input('Продолжить: ')
+                if sequel == 'no':
+                    key_add = False
+                print()
+    except:
+        print('Телефонного справочника нет')  
 
-# def search_writing():
-#     return
+def search_writing(key_search):
+    if key_search == 's':
+        value = input_surname()
+        ind = 0
+    elif key_search == 'n':
+        value = input_firstname()
+        ind = 1
+    else:
+        value = input_num_phone()
+        ind = 3
+    print()
+    print('Результат:')
+    try:
+        with open('my_phone_book.txt', 'r', encoding='utf-8') as phone_book:
+            result = []
+            for writing in phone_book:
+                writing = writing.split('!')
+                if value.strip().lower() == writing[ind].strip().lower():
+                    phone_book.tell()
+                    writing.append()
+                    result.append(tuple(writing))
+                    # print(f'Фамилия: {writing[0]}')
+                    # print(f'Имя: {writing[1]}')
+                    # print(f'Отчество: {writing[2]}')
+                    # print(f'Номер телефона: {writing[3]}')
+        return result
+    except:
+        print('Телефонного справочника нет')
 
 # def change_writing():
 #     return
--- Task38/ui.py
+++ Task38/ui.py
@@ -3,12 +3,21 @@ def hint():
     print('Какое действие желаете выполнить:' + '\n' + 
         '\t' + '1) импортировать данные в телефонный справочник (введите "i")' + '\n' +
         '\t' + '2) просмотр телефонного справочника (введите "v")' + '\n' +
-        '\t' + '3) поиск информации по фамилии (введите "f")' + '\n' +
+        '\t' + '3) поиск (введите "f")' + '\n' +
         '\t' + '4) добавить запись в телефонный справочник (введите "a")' + '\n' +
-        '\t' + '5) экспортировать данные из телефонного справочника (введите "u")' + '\n' +
+        '\t' + '5) экспортировать данные из телефонного справочника (введите "o")' + '\n' +
         '\t' + '6) выйти из телефонного справочника (введите "e")')
     print()
     return input('Введите действие: ').lower()
+
+def hint_find():
+    print('Выберите тип поиска:' + '\n' + 
+        '\t' + '1) по фамилии (введите "s")' + '\n' +
+        '\t' + '2) по имени (введите "n")' + '\n' +
+        '\t' + '3) по номеру телефона (введите "t")' + '\n' +
+        '\t' + '4) завершить поиск (введите "e")')
+    print()   
+    return input('Искать по: ').lower()
 
 def interface():
     print('<<< Телефонный справочник >>>')
@@ -16,31 +25,33 @@ def interface():
     while key != 'e':        
         if key == 'i':
             import_writing()
-            print() 
+            input('Для продолжения, нажмите "Enter"')
         elif key == 'v':
             viewer_writing()
-            print()
-        # elif action == 'f':
-        #     surname = input('Введите фамилию: ')
-        #     find_info_surname(surname)
-        # elif action == 'i':
-        #     file_import = input('Введите имя файла с расширением для импортирования данных: ')
-        #     path_file_imp = input('Укажите путь к файлу: ')
-        #     try:
-        #         import_info(path_file_imp, file_import)
-        #     except:
-        #         print('По указанному пути нет нужного файла')
-        # elif action == 'u':
-        #     file_export = input('Введите имя файла с расширением для экспортирования данных: ')
-        #     path_file_exp = input('Укажите путь к файлу: ')
-        #     unload_info(path_file_exp, file_export)
-        # elif key == 'e':
-        #     print('До свидания!!!')
+            input('Для продолжения, нажмите "Enter"')
+        elif key == 'f':
+            key_f = hint_find()
+            while key_f != 'e':
+                if key_f in ('s', 'n', 't'):
+                    result_search = search_writing(key_f)
+                    if len(result_search) != 0:
+                        print(*result_search)
+                    else:
+                        print('Ничего не найдено!!!', end='\n\n')
+                    input('Для продолжения, нажмите "Enter"')
+                else:
+                    print('Введено недопустимое значение', end='\n\n')
+                key_f = hint_find()
+            input('Для продолжения, нажмите "Enter"')
+        elif key == 'a':
+            add_writing(True)
+            input('Для продолжения, нажмите "Enter"')
+        elif key == 'o':
+            export_writing()
+            input('Для продолжения, нажмите "Enter"')
         else:
-            print('Введено недопустимое значение')
-            print()
+            print('Введено недопустимое значение', end='\n\n')
         key = hint()
-        if key == 'e':
-            print('До свидания!!!')
+    print('До свидания!!!')
 
 interface()
