6259f2891014c4f9c8650764bff830af989d33a4
DureginKostya <kot-502@mail.ru>
2023-04-04 14:10:29 +0500

    Added file create_person.py and folder Task38

---
+++ Task38/create_person.py
@@ -0,0 +1,11 @@
+def input_surname():
+    return input('Фамилию: ')
+
+def input_firstname():
+    return input('Имя: ')
+
+def input_patronymic():
+    return input('Отчество: ')
+
+def input_num_phone():
+    return input('Номер телефона: ')
