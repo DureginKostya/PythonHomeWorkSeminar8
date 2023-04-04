96a70a9a8a689afd36644df8a7ccfbb1ea655779
DureginKostya <kot-502@mail.ru>
2023-04-04 14:25:10 +0500

    Rename file create_person.py -> create_data.py and added file processing_writing.py

---
+++ Task38/processing_writing.py
@@ -0,0 +1,21 @@
+from create_person import *
+def import_writing():
+    return
+
+def export_writing():
+    return
+
+def viewer_writing():
+    return
+
+def add_writing():
+    return
+
+def search_writing():
+    return
+
+def change_writing():
+    return
+
+def del_writing():
+    return
